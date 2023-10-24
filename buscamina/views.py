c497a3e299d70dc5e539989a8ea6a26b531a1de0
Manuel <mmarrui1802@iesmartinezm.es>
2023-10-24 13:37:59 +0200

    Se le puede pasar parámetros por URL

--- buscamina/views.py
+++ buscamina/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import Post
+
 
 def index(request):
     return render(request, 'buscamina/index.html', {})
 
 def crea_tablero(request):
-    return render(request, 'buscamina/crea_tablero.html', {})
+    filas =     range(1, int( request.GET.get("filas", 0))+1)  # 0 es el valor predeterminado si no se proporciona "filas"
+    columnas =  range(1, int( request.GET.get("columnas", 0))+1)  # 0 es el valor predeterminado si no se proporciona "columnas"
+    return render(request, 'buscamina/crea_tablero.html', {"filas": filas, "columnas": columnas})
 
 def drawTable(request):
     return render(request, 'buscamina/draw_table.py')
